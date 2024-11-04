33c03ab20e3212a2302cb6e5305af6ed017e8a3b
Bảo Nguyễn <baonguyen@MacBook-Pro-cua-Bao-Bao.local>
2024-11-04 16:57:33 +0700

    schedule API done

--- lib/screens/schedule_screen.dart
+++ lib/screens/schedule_screen.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:sap_mobile/screens/home_screen.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ScheduleScreen extends StatefulWidget {
   @override
@@ -8,162 +10,312 @@ class ScheduleScreen extends StatefulWidget {
 
 class _ScheduleScreenState extends State<ScheduleScreen> {
   int _selectedDayIndex = 0;
+  DateTime _currentWeekStartDate = DateTime(2025, 1, 6); // Bắt đầu từ thứ Hai, ngày 6/1/2025
+  List<Map<String, dynamic>> _schedule = [];
+  String? currentUserId;
+  Map<int, String> courseLocations = {};
+  List<Map<String, dynamic>> filteredSchedule = [];
 
-  List<String> _days = ['21', '22', '23', '24', '25', '26', '27'];
-  List<String> _weekDays = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
-  List<Map<String, String>> _schedule = [
-    {
-      'time': '11:35 13:05',
-      'title': 'C_TS462_1',
-      'location': 'Online 100',
-      'details': 'Google Meet',
-    },
-    {
-      'time': '13:15 14:45',
-      'title': 'C_TS462_2',
-      'location': 'Offline 111',
-      'details': 'Room 111',
-    },
-    // Thêm các buổi học khác ở đây
-  ];
+  @override
+  void initState() {
+    super.initState();
+    _loadUserId();
+  }
+
+  Future<void> _loadUserId() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      currentUserId = prefs.getString('currentUserId');
+    });
+
+    if (currentUserId != null) {
+      checkEnrollmentStatus();
+    } else {
+      print("User ID not found in SharedPreferences");
+    }
+  }
+
+  Future<void> checkEnrollmentStatus() async {
+    if (currentUserId == null) {
+      print("User ID is null, cannot check enrollment status.");
+      return;
+    }
+
+    try {
+      final enrollmentResponse = await http.get(
+          Uri.parse('https://swdsapelearningapi.azurewebsites.net/api/Enrollment/get-all'));
+      if (enrollmentResponse.statusCode == 200) {
+        final data = json.decode(enrollmentResponse.body);
+        final List enrollments = data is Map && data.containsKey('\$values')
+            ? data['\$values']
+            : (data is List ? data : [data]);
+
+        print("Fetched enrollments data: $enrollments");
+
+        final enrollment = enrollments.firstWhere(
+          (enrollment) =>
+              enrollment['userId'] == currentUserId &&
+              enrollment['status'] == 'Success',
+          orElse: () {
+            print("User không có enrollment status 'Success' hoặc không tìm thấy userId phù hợp.");
+            return null;
+          },
+        );
+
+        if (enrollment != null) {
+          final courseId = enrollment['courseId'];
+          await fetchCourseLocation(courseId);
+          await fetchCourseSessions(courseId);
+        } else {
+          print("Không tìm thấy enrollment với status 'Success' cho user hiện tại.");
+        }
+      } else {
+        throw Exception('Failed to load enrollment data');
+      }
+    } catch (e) {
+      print("Error fetching enrollment data: $e");
+    }
+  }
+
+  Future<void> fetchCourseLocation(int courseId) async {
+    try {
+      final courseResponse = await http.get(
+          Uri.parse('https://swdsapelearningapi.azurewebsites.net/api/Course/get-all'));
+      if (courseResponse.statusCode == 200) {
+        final data = json.decode(courseResponse.body);
+        final List courses = data is Map && data.containsKey('\$values') ? data['\$values'] : [data];
+
+        for (var course in courses) {
+          if (course['id'] == courseId) {
+            setState(() {
+              courseLocations[courseId] = course['location'] ?? 'No Location';
+            });
+            break;
+          }
+        }
+      } else {
+        throw Exception('Failed to load course data');
+      }
+    } catch (e) {
+      print("Error fetching course location data: $e");
+    }
+  }
+
+  Future<void> fetchCourseSessions(int courseId) async {
+    try {
+      final courseSessionResponse = await http.get(
+          Uri.parse('https://swdsapelearningapi.azurewebsites.net/api/CourseSession/get-all'));
+      if (courseSessionResponse.statusCode == 200) {
+        final data = json.decode(courseSessionResponse.body);
+        final List courseSessions = data is Map && data.containsKey('\$values') ? data['\$values'] : [data];
+
+        final sessions = courseSessions.where((session) => session['courseId'] == courseId).toList();
+
+        setState(() {
+          _schedule = sessions.map((session) {
+            return {
+              'title': session['courseName'] ?? 'No Title',
+              'sessionName': session['sessionName'] ?? 'No Session Name',
+              'sessionDate': DateTime.parse(session['sessionDate']),
+              'location': courseLocations[courseId] ?? 'No Location',
+            };
+          }).toList();
+          filterSessionsBySelectedDay();
+        });
+        print("Schedule loaded: $_schedule");
+      } else {
+        throw Exception('Failed to load course session data');
+      }
+    } catch (e) {
+      print("Error fetching course session data: $e");
+    }
+  }
+
+  void filterSessionsBySelectedDay() {
+    DateTime selectedDate = _currentWeekStartDate.add(Duration(days: _selectedDayIndex));
+    setState(() {
+      filteredSchedule = _schedule
+          .where((session) => session['sessionDate'] == selectedDate)
+          .toList();
+    });
+  }
 
   void _selectDay(int index) {
     setState(() {
       _selectedDayIndex = index;
     });
+    filterSessionsBySelectedDay();
+  }
+
+  void _previousWeek() {
+    setState(() {
+      _currentWeekStartDate = _currentWeekStartDate.subtract(Duration(days: 7));
+      _selectedDayIndex = 0;
+    });
+    filterSessionsBySelectedDay();
+  }
+
+  void _nextWeek() {
+    setState(() {
+      _currentWeekStartDate = _currentWeekStartDate.add(Duration(days: 7));
+      _selectedDayIndex = 0;
+    });
+    filterSessionsBySelectedDay();
   }
 
   @override
   Widget build(BuildContext context) {
+    List<String> _weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: false, // Xóa mũi tên quay lại
-        title: Text(
-          'Schedule',
+      title: Center(
+        child: Text(
+          'Weekly Timetable',
           style: TextStyle(
-            color: Colors.black,
-            fontSize: 24,
+            fontSize: 25,
             fontWeight: FontWeight.bold,
           ),
         ),
       ),
+      automaticallyImplyLeading: false,
+    ),
       body: Column(
         children: [
-          // Horizontal Day Selector
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: _previousWeek,
+              ),
+              Text(
+                'Jan 2025',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              IconButton(
+                icon: Icon(Icons.arrow_forward),
+                onPressed: _nextWeek,
+              ),
+            ],
+          ),
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: _days
-                  .asMap()
-                  .entries
-                  .map(
-                    (entry) => GestureDetector(
-                      onTap: () => _selectDay(entry.key),
-                      child: Container(
-                        padding: EdgeInsets.all(8.0),
-                        decoration: BoxDecoration(
-                          color: _selectedDayIndex == entry.key
-                              ? Color(0xFF275998)
-                              : Colors.transparent,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Column(
-                          children: [
-                            Text(
-                              _weekDays[entry.key],
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: _selectedDayIndex == entry.key
-                                    ? Colors.white
-                                    : Colors.black,
-                              ),
-                            ),
-                            Text(
-                              entry.value,
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                                color: _selectedDayIndex == entry.key
-                                    ? Colors.white
-                                    : Colors.black,
-                              ),
-                            ),
-                            if (_selectedDayIndex == entry.key)
-                              Container(
-                                height: 4,
-                                width: 24,
-                                color: Colors.white,
-                              ),
-                          ],
-                        ),
-                      ),
+              children: List.generate(7, (index) {
+                DateTime day = _currentWeekStartDate.add(Duration(days: index));
+                return GestureDetector(
+                  onTap: () => _selectDay(index),
+                  child: Container(
+                    padding: EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      color: _selectedDayIndex == index
+                          ? Color(0xFF275998)
+                          : Colors.transparent,
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                  )
-                  .toList(),
-            ),
-          ),
-          // Time and Course Rows
-          Expanded(
-            child: ListView.builder(
-              itemCount: _schedule.length,
-              itemBuilder: (context, index) {
-                final item = _schedule[index];
-                return Row(
-                  children: [
-                    // Time Column
-                    Container(
-                      padding: EdgeInsets.all(16),
-                      width: 100,
-                      child: Text(
-                        item['time']!,
-                        style: TextStyle(fontSize: 16),
-                      ),
-                    ),
-                    // Course Details Column
-                    Expanded(
-                      child: Card(
-                        color: Colors.white, // Đổi màu nền của Card
-                        margin: EdgeInsets.symmetric(vertical: 8.0),
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                item['title']!,
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black, // Đổi màu chữ thành trắng
-                                ),
-                              ),
-                              SizedBox(height: 8),
-                              Row(
-                                children: [
-                                  Icon(Icons.location_on, size: 16, color: Colors.black),
-                                  SizedBox(width: 4),
-                                  Text(
-                                    item['location']!,
-                                    style: TextStyle(color: Colors.black), // Đổi màu chữ thành trắng
-                                  ),
-                                ],
-                              ),
-                              SizedBox(height: 8),
-                              Text(
-                                item['details']!,
-                                style: TextStyle(color: Colors.black), // Đổi màu chữ thành trắng
-                              ),
-                            ],
+                    child: Column(
+                      children: [
+                        Text(
+                          _weekDays[index],
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: _selectedDayIndex == index
+                                ? Colors.white
+                                : Colors.black,
                           ),
                         ),
-                      ),
+                        Text(
+                          day.day.toString(),
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: _selectedDayIndex == index
+                                ? Colors.white
+                                : Colors.black,
+                          ),
+                        ),
+                        if (_selectedDayIndex == index)
+                          Container(
+                            height: 4,
+                            width: 24,
+                            color: Colors.white,
+                          ),
+                      ],
                     ),
-                  ],
+                  ),
                 );
-              },
+              }),
             ),
+          ),
+          Expanded(
+            child: filteredSchedule.isNotEmpty
+                ? ListView.builder(
+                    itemCount: filteredSchedule.length,
+                    itemBuilder: (context, index) {
+                      final item = filteredSchedule[index];
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 8.0, horizontal: 16.0),
+                        child: Card(
+                          color: Colors.white,
+                          margin: EdgeInsets.zero,
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  item['title']!,
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                SizedBox(height: 8),
+                                Text(
+                                  item['sessionName']!,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black54,
+                                  ),
+                                ),
+                                SizedBox(height: 8),
+                                Row(
+                                  children: [
+                                    Icon(Icons.calendar_today,
+                                        size: 18, color: Colors.black),
+                                    SizedBox(width: 4),
+                                    Text(
+                                      "${item['sessionDate'].year}-${item['sessionDate'].month.toString().padLeft(2, '0')}-${item['sessionDate'].day.toString().padLeft(2, '0')}",
+                                      style: TextStyle(
+                                          color: Colors.black, fontSize: 16),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(height: 8),
+                                Row(
+                                  children: [
+                                    Icon(Icons.location_on,
+                                        size: 18, color: Colors.black),
+                                    SizedBox(width: 4),
+                                    Text(
+                                      item['location']!,
+                                      style: TextStyle(
+                                          color: Colors.black, fontSize: 16),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  )
+                : Center(child: Text("No schedule available")),
           ),
         ],
       ),
